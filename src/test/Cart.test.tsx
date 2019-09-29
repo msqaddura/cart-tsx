e20311274052499f546a4f948e121cc84980b0eb
Mohamad Qaddura <msqaddura@gmail.com>
2019-09-29 21:39:52 +0300

    fixed unit tests

--- src/test/Cart.test.tsx
+++ src/test/Cart.test.tsx
@@ -4,11 +4,13 @@ import renderer, { ReactTestRenderer } from 'react-test-renderer'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 import { Store, AnyAction } from 'redux'
+import { ThemeProvider } from 'emotion-theming'
 import CartContainer from '../containers/CartContainer'
 
 import { MATCHES } from '../store/matches/mock'
 import { addItemToCart } from '../store/cart/actions'
 import { ApplicationState } from '../store'
+import { main } from '../styles/themes'
 
 const mockStore = configureStore([])
 describe('CartContainer Connected to Redux', () => {
@@ -20,9 +22,12 @@ describe('CartContainer Connected to Redux', () => {
       cart: { data: [], total: 0 }
       // cart: { data: [{ stake: 0, match: MATCHES[0], anticipated: MATCHES[0].participants[0].name }], total: 0 }
     })
+    // yes we do need to pass theme provider along with store
     component = renderer.create(
       <Provider store={store}>
-        <CartContainer />
+        <ThemeProvider theme={main}>
+          <CartContainer />
+        </ThemeProvider>
       </Provider>
     )
   })
@@ -34,7 +39,7 @@ describe('CartContainer Connected to Redux', () => {
   it('should render no-cart', () => {
     store.dispatch(addItemToCart({ match: MATCHES[1], anticipated: MATCHES[1].participants[1] }))
     // https://github.com/dmitry-zaets/redux-mock-store/issues/71
-    // as you can see, the store did not get changed, bcz redux-mock-store doesnt work on reducer level
+    // a s you can see, the store did not get changed, bcz redux-mock-store doesnt work on reducer level
     expect(component.root.findByType(CartContainer.WrappedComponent).findAllByProps({ className: 'filled' })).toHaveLength(0)
   })
 })
